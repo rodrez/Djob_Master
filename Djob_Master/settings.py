f9ef0d45311cd3b6c5e425cc501156a25cd8e1a8
Rodrez <fabian.rodrez@gmail.com>
2021-10-24 10:03:04 -0400

    Change storage type

--- Djob_Master/settings.py
+++ Djob_Master/settings.py
@@ -178,7 +178,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "storage.WhiteNoiseStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Media
